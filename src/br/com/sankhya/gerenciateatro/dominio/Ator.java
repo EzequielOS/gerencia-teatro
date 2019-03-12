977c159bacce3fd0397340f9e93f506d2a0ec37d
Ezequiel Oliveira <ezequiel.santana@sankhya.com.br>
2019-03-12 15:06:34 -0300

    Cria classe Ator e implementa seus atributos basicos

---
+++ src/br/com/sankhya/gerenciateatro/dominio/Ator.java
@@ -0,0 +1,17 @@
+/*
+Nome do autor: Ezequiel de Oliveira Sant'Ana
+Data de criacaoo do arquivo: 08/03/2019
+Objetivo sucinto da classe: Cria classe Ator que implementa relacionamento com Papel
+Escopo do projeto: https://docs.google.com/document/d/1ewS7W5Lacoxjorj8NxD3zc2uH32P5fWOu2xqjVTWym0/edit
+*/
+
+package br.com.sankhya.gerenciateatro.dominio;
+
+import java.math.BigDecimal;
+
+public class Ator {
+	private Integer ID;
+	private String nome;
+	private String nacionalidade;
+	private BigDecimal salario;
+}
